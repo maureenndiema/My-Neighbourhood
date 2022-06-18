3e909c6a7be4b77fbdc0c7e5bb1faf43892688d1
maureenndiema <ndiemam@gmail.com>
2022-06-18 15:57:22 +0300

    creating name.location,admin in class neoghbourhood in models.py

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class NeighbourHood(models.Model):
-    
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo = CloudinaryField('images')
+    description = models.TextField()
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+
