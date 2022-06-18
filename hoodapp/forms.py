2f1dc59e5c7eb6d222bc961b840ffd1dfda9b594
maureenndiema <ndiemam@gmail.com>
2022-06-18 17:51:42 +0300

    addind class businessform and postform in forms.py

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -21,3 +21,14 @@ class NeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
         exclude = ('admin',)
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
