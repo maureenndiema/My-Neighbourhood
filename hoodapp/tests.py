9c605f330048a41788e5a4c38eec935546bc71e9
maureenndiema <ndiemam@gmail.com>
2022-06-18 16:19:28 +0300

    import user ,tetscase,auth in test.py

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -1,3 +1,7 @@
+from django import test
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your tests here.
