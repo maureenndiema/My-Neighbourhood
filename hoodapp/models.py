6bc147ddb23a9893141f6cf349079cf9fce8ebc5
maureenndiema <ndiemam@gmail.com>
2022-06-18 16:16:06 +0300

    adding name,email,description in profile in class profile

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -49,6 +49,12 @@ class Profile(models.Model):
         instance.profile.save()
 
 class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
 
 
 
