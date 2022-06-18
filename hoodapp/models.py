327ed31baac311a1fd594688028770a3947908f0
maureenndiema <ndiemam@gmail.com>
2022-06-18 16:10:51 +0300

    function for creating and deleting neighborhood

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -15,4 +15,18 @@ class NeighbourHood(models.Model):
     health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+
 
